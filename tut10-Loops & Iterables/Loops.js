46c9156048a1d2102547e8bc366664d2840ba1ed
Balesh <baleshvarshney078@gmail.com>
2025-10-13 22:10:18 +0530

    Loops

--- tut10-Loops & Iterables/Loops.js
+++ tut10-Loops & Iterables/Loops.js
@@ -1,7 +1,14 @@
 // ------------------------------ Loops ------------------------------
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-for (let i =0 ; i < arr.length; i++){
-    console.log(arr[i])
+// for (let i =0 ; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+const arr1 = ["balesh" , "Gyanesh" , "aditya"]
+let text = " "
+
+for (let i = 0 ; i < arr1.length ;  i++){
+    console.log(text += arr1[i])
 }
