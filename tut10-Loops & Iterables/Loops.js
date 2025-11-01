0242a392963d1304c03829d44b1facb0a7305da9
Balesh <baleshvarshney078@gmail.com>
2025-11-01 14:59:38 +0530

    modified loops

--- tut10-Loops & Iterables/Loops.js
+++ tut10-Loops & Iterables/Loops.js
@@ -1,14 +1,44 @@
-// ------------------------------ Loops ------------------------------
+// -------------------------------------  Loops.js  -------------------------------------
+// This file contains examples of different types of loops in JavaScript.
 
-// const arr = [1, 2, 3, 4, 5];
+// Example of a 'for' loop 
 
-// for (let i =0 ; i < arr.length; i++){
-//     console.log(arr[i])
-// }
-
-const arr1 = ["balesh" , "Gyanesh" , "aditya"]
-let text = " "
-
-for (let i = 0 ; i < arr1.length ;  i++){
-    console.log(text += arr1[i])
+for (let i=0; i<=10; i++){
+    console.log(i)
 }
+
+let cars = ["Toyota", "Honda", "Ford", "BMW", "Mercedes"];
+for (let i = 0; i<cars.length; i++){
+    console.log(cars[i])
+}
+
+
+// Example of a 'while' loop
+
+let j = 0;
+while (j <= 10){
+    console.log(j);
+    j++;
+}
+
+let myarr = ["Apple", "Banana", "Cherry", "Date"];
+let k = 0;
+while (k < myarr.length){
+    console.log(myarr[k]);
+    k++;
+}
+
+// Example of a 'do...while' loop
+
+let m = 0;
+do {
+    console.log(m);
+    m++;
+} while (m <= 10);
+
+let fruits = ["Mango", "Pineapple", "Grapes", "Orange"];
+let n = 0;
+do {
+    console.log(fruits[n]);
+    n++;
+} while (n < fruits.length);
