aa804dc67302d26e45da17717249ba3197bfdba8
Balesh <baleshvarshney078@gmail.com>
2025-11-21 21:13:43 +0530

    Functiom

---
+++ tut18-Functions/Functions.js
@@ -0,0 +1,7 @@
+//*=============================
+//*   Function in Javascript 
+//* =============================
+
+//? Functions are reusable block of code designed to perform a particular task. Functions are executed when they are "called" or "invoked".
+
+// ^ Example1 ==  
